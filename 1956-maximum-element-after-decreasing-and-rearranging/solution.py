33143f41c6cfc281e2c858d7a21ade1111575472
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2023-11-16 06:53:56 +0000

    Sync LeetCode submission Runtime - 366 ms (5.04%), Memory - 21.5 MB (11.34%)

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        arr.sort()
+        arr[0]=1
+        for i in range(0,len(arr)-1):
+            if((arr[i+1]-arr[i])>1):
+                arr[i+1]=arr[i]+1
+        return(arr[-1])
