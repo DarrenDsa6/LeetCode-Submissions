6ae67082e5def867a018883d54085616d5c740d2
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2024-07-13 19:46:51 +0000

    Sync LeetCode submission Runtime - 43 ms (5.01%), Memory - 16.8 MB (100.00%)

---
+++ 2551-apply-operations-to-an-array/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        last_non_zero_index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[last_non_zero_index] = nums[i]
+                last_non_zero_index += 1
+        # Fill the rest with zeros
+        for i in range(last_non_zero_index, len(nums)):
+            nums[i] = 0
+        return nums
+        
