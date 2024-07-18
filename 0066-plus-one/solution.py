78cc1280a17a2fe82345779207d1f17600cc8b23
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2024-07-18 06:38:57 +0000

    Sync LeetCode submission Runtime - 36 ms (0.02%), Memory - 16.6 MB (100.00%)

---
+++ 0066-plus-one/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] = digits[-1] + 1
+        if digits[-1] == 10:
+            i = len(digits)-1
+            while i>0 and digits[i] == 10:
+                digits[i] = 0
+                digits[i-1] = digits[i-1]+1
+                i -= 1
+            if digits[0] == 10:
+                digits[0] = 0
+                digits.insert(0, 1)
+        return digits
