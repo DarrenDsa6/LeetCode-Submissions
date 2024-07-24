688077eb7e162fc097e9016c994c3a3260e1486d
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2024-07-24 17:38:06 +0000

    Sync LeetCode submission Runtime - 52 ms (100.00%), Memory - 16.6 MB (100.00%)

---
+++ 0080-remove-duplicates-from-sorted-array-ii/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        slow = 0
+        count = 1
+        for fast in range(1, len(nums)):
+            if nums[fast] == nums[slow]:
+                count += 1
+            else:
+                count = 1
+            if count <= 2:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow + 1 
