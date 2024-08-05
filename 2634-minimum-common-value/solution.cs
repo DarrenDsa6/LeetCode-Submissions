23fc02f0469d75cf9e50dd051769b72c6bcb1256
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2024-08-05 18:39:05 +0000

    Sync LeetCode submission Runtime - 229 ms (10.31%), Memory - 65.8 MB (100.00%)

---
+++ 2634-minimum-common-value/solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        while (i<nums1.Length && j<nums2.Length){
+            if (nums1[i] == nums2[j]){
+                return nums1[i];
+            }
+            if(nums1[i] < nums2[j]) i += 1; else j += 1;
+        }
+        return -1;
+    }
+}
