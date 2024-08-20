d7f8e33b61bf8b5b7ba79d169fe64a2bebd0b358
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2024-08-20 18:30:40 +0000

    Sync LeetCode submission Runtime - 141 ms (7.14%), Memory - 51.3 MB (100.00%)

---
+++ 2454-largest-local-values-in-a-matrix/solution.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[][] LargestLocal(int[][] grid) {
+        int n = grid.Length;
+        int[][] maxLocal = new int[n - 2][];
+        for (int i = 0; i < n - 2; i++) {
+            maxLocal[i] = new int[n - 2];
+            for (int j = 0; j < n - 2; j++) {
+                maxLocal[i][j] = FindMaxInSubmatrix(grid, i, j);
+            }
+        }
+        return maxLocal;
+    }
+    private int FindMaxInSubmatrix(int[][] grid, int row, int col) {
+        int max = int.MinValue;
+        for (int i = row; i < row + 3; i++) {
+            for (int j = col; j < col + 3; j++) {
+                max = Math.Max(max, grid[i][j]);
+            }
+        }
+        return max;
+    }
+}
