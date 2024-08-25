be2343d23498661b3f0d4820ebcd005ea6c03d44
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2024-08-25 13:10:35 +0000

    Sync LeetCode submission Runtime - 61 ms (5.45%), Memory - 44.5 MB (100.00%)

---
+++ 1651-shuffle-string/solution.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string RestoreString(string s, int[] indices) {
+        StringBuilder answer = new StringBuilder(s);
+        int j = 0;
+        foreach(int i in indices){
+            answer[i] = s[j++];
+        }
+        return answer.ToString();
+    }
+}
