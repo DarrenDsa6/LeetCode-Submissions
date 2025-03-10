6285b0ec6c17abe625ca62f41b34f488978e8089
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2025-03-10 05:10:16 +0000

    Sync LeetCode submission Runtime - 13 ms (75.44%), Memory - 56.2 MB (100.00%)

--- 0217-contains-duplicate/solution.cs
+++ 0217-contains-duplicate/solution.cs
@@ -1,14 +1,5 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        HashSet<int> elements = new HashSet<int>();
-        foreach(int i in nums){
-            if (elements.Contains(i)){
-                return true;
-            }
-            else{
-                elements.Add(i);
-            }
-        }
-        return false;
+        return new HashSet<int>(nums).Count() < nums.Length;
     }
 }
