e3656b2ab5c3b270c2ec4af7305d4bfc5d5d27ad
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2025-05-03 10:24:39 +0000

    Sync LeetCode submission Runtime - 1 ms (29.99%), Memory - 45.6 MB (99.94%)

--- 0268-missing-number/solution.java
+++ 0268-missing-number/solution.java
@@ -1,11 +1,29 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int numsLen = nums.length;
-        int totalSum= numsLen*(numsLen+1)/2;
-        int sum = 0;
-        for(int num: nums){
-            sum += num;
+        int i = 0;
+        while (i < nums.length) {
+            // Ensure we're placing numbers in their correct positions
+            if (nums[i] < nums.length && nums[i] != i && nums[i] != nums[nums[i]]) {
+                swap(nums, i, nums[i]);
+            } else {
+                i++;
+            }
         }
-        return (totalSum - sum);
+
+        // Find the missing number
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+
+        return nums.length; // If everything is correctly placed, missing number is 'n'
+    }
+
+    static void swap(int[] nums, int i, int correctIndex) {
+        int temp = nums[i];
+        nums[i] = nums[correctIndex];
+        nums[correctIndex] = temp;
     }
 }
+
