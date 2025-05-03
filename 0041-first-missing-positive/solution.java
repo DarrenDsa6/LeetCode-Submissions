450a5021557ccbfc1048d3e3123392b27aa91b74
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2025-05-03 18:53:35 +0000

    Sync LeetCode submission Runtime - 1 ms (99.99%), Memory - 57.6 MB (99.95%)

---
+++ 0041-first-missing-positive/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i<nums.length){
+            if(nums[i]<nums.length && nums[i]>0 &&nums[i] != nums[nums[i] - 1]){
+                swap(nums, i, nums[i]-1);
+            }
+            else{
+                i++;
+            }
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!= i+1){
+                return i+1;
+            }
+        }
+        return i+1;
+    }
+
+    private void swap(int[] nums, int i, int correctIndex) {
+        int temp = nums[i];
+        nums[i] = nums[correctIndex];
+        nums[correctIndex] = temp;
+    }
+}
