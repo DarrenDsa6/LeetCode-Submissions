7da12fc65566206c3af5b8179fe2fccbbfe1a975
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2025-05-17 17:50:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.8 MB (99.95%)

---
+++ 0059-spiral-matrix-ii/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] ans = new int[n][n];
+        int left = 0;
+        int right = n-1;
+        int bottom = n-1;
+        int top = 0;
+        int count = 1;
+        while(left<=right && top<=bottom){
+            for(int i=left; i<=right;i++){
+                ans[top][i] = count++;
+            }
+            top += 1;
+            for(int i=top; i<=bottom; i++){
+                ans[i][right] = count++;
+            }
+            right -= 1;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans[bottom][i] = count++;
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans[i][left] = count++;
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
