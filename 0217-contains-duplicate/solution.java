95bcdc324a7ed458fdd9e774a5e060c5e0dfbfd8
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2025-08-25 16:37:28 +0000

    Sync LeetCode submission Runtime - 13 ms (94.28%), Memory - 58.2 MB (99.91%)

--- 0217-contains-duplicate/solution.java
+++ 0217-contains-duplicate/solution.java
@@ -1,12 +1,12 @@
-import java.util.*;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> checkDuplicates = new HashSet<>();
-
-        for (int num : nums) {
-            if (!checkDuplicates.add(num)) { 
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            if(set.contains(num)){
                 return true;
+            }
+            else{
+                set.add(num);
             }
         }
         return false;
