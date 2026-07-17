37ae884d0de9c439315d47740b8c03dde2184c4b
DarrenDsa6 <109817881+DarrenDsa6@users.noreply.github.com>
2026-07-17 16:13:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (77.73%)

--- 0033-search-in-rotated-sorted-array/solution.py
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start, end = 0, len(nums) - 1
@@ -7,18 +5,14 @@ class Solution:
             mid = (start + end) // 2
             if nums[mid] == target:
                 return mid
-            
-            # Check if left half is sorted
             if nums[start] <= nums[mid]:
                 if nums[start] <= target < nums[mid]:
                     end = mid - 1
                 else:
                     start = mid + 1
             else:
-                # Right half is sorted
                 if nums[mid] < target <= nums[end]:
                     start = mid + 1
                 else:
                     end = mid - 1
         return -1
-
